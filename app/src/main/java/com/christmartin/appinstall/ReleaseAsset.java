7b9630636feb1477378e434edc945a1d7714910c
ChrisMartin00 <martinchris06@iCloud.com>
2026-04-28 13:24:40 -0400

    Add release asset model

---
+++ app/src/main/java/com/christmartin/appinstall/ReleaseAsset.java
@@ -0,0 +1,11 @@
+package com.christmartin.appinstall;
+
+final class ReleaseAsset {
+    final String name;
+    final String downloadUrl;
+
+    ReleaseAsset(String name, String downloadUrl) {
+        this.name = name;
+        this.downloadUrl = downloadUrl;
+    }
+}
